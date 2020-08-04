be5ae4e114c3e38c68a4463074e6171d914827ec
Renan Rodrigues <renanfr1047@gmail.com>
2020-08-04 20:53:59 -0300

    first commit

---
+++ src/main/java/br/com/facilpay/config/server/FacilpayConfigServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.facilpay.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacilpayConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacilpayConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/facilpay/config/server/FacilpayConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.facilpay.config.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FacilpayConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
