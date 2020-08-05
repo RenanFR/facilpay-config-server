88ca461ff659a90b7eeb7f89df146ce129336fff
Renan Rodrigues <renanfr1047@gmail.com>
2020-08-05 10:55:30 -0300

    Configuration server setup

--- src/main/java/br/com/facilpay/config/server/FacilpayConfigServerApplication.java
+++ src/main/java/br/com/facilpay/config/server/FacilpayConfigServerApplication.java
@@ -2,8 +2,10 @@ package br.com.facilpay.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class FacilpayConfigServerApplication {
 
 	public static void main(String[] args) {
